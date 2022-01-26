a7f2a082d0da89cbc202be135426d16c8f57f4f3
Gansonre Ismael <proismaelgans2017@gmail.com>
2022-01-26 04:07:13 -0500

    try to fix code

--- lib/ui/screens/login.dart
+++ lib/ui/screens/login.dart
@@ -1,3 +1,4 @@
+import 'package:firfood/google_sign_in_button.dart';
 import 'package:flutter/material.dart';
 
 class Login extends StatelessWidget {
@@ -6,28 +7,43 @@ class Login extends StatelessWidget {
     // Private methods within build method help us to
     // organize our code and recognize structure of widget
     // that we're building:
+    BoxDecoration _buildBackground() {
+      return const BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage("assets/splash.png"),
+          fit: BoxFit.cover,
+        ),
+      );
+    }
+
     Text _buildText() {
       return Text(
-        'Plats Africains',
+        'App name ',
+        style: TextStyle(
+          fontFamily: 'MerryWeather',
+          fontSize: 40.0,
+          color: const Color(0xFF00C6FF),
+        ),
         textAlign: TextAlign.center,
       );
     }
 
     return Scaffold(
-      backgroundColor: Colors.greenAccent,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            _buildText(),
-            // Space between "Recipes" and the button:
-            SizedBox(height: 50.0),
-            MaterialButton(
-              color: Colors.white,
-              child: Text("Se connecter avec  Google"),
-              onPressed: () => print("Button pressed."),
-            )
-          ],
+      // We do not use backgroundColor property anymore.
+      // New Container widget wraps our Center widget:
+      body: Container(
+        decoration: _buildBackground(),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              _buildText(),
+              SizedBox(height: 50.0),
+              GoogleSignInButton(
+                onPressed: () => print("Button pressed."),
+              ),
+            ],
+          ),
         ),
       ),
     );
